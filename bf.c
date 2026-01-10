a0f1876abecaa420254dc2eba8e83211bb90a6a4
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2026-01-10 23:47:24 +0100

    refactor: revamped complete interpreter

--- bf.c
+++ bf.c
@@ -1,142 +1,124 @@
-/*** INCLUDES ***/
+/* ================================ INCLUDES ================================ */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <stdint.h>
 
-/*** DEFINES ***/
+/* =============================== CONSTANTS ================================ */
 
-#define DATA_SIZE 30000
-#define INSTRUCTIONS_SIZE 2048
+#define TAPE_SIZE 30000
+#define LOOP_STACK_SIZE 1024
 
-/*** ERROR HANDLING ***/
+/* ================================== EVAL ================================== */
 
-void die(const char *s) {
-  perror(s);
-  exit(1);
+void bf_eval(uint8_t *tape, size_t ptr, char *instructions, size_t instruction_len)
+{
+	size_t loop_stack[LOOP_STACK_SIZE];
+	int loop_stack_ptr = -1;
+	size_t instruction_ptr = 0;
+
+	while (instruction_ptr < instruction_len) {
+		switch(instructions[instruction_ptr]) {
+		case '+':
+			++tape[ptr];
+			break;
+		case '-':
+			--tape[ptr];
+			break;
+		case '>':
+			if (TAPE_SIZE - 1 == ptr) {
+				ptr = 0;
+			} else {
+				++ptr;
+			}
+			break;
+		case '<':
+			if (0 == ptr) {
+				ptr = TAPE_SIZE - 1;
+			} else {
+				--ptr;
+			}
+			break;
+		case '.':
+			putc(tape[ptr], stdout);
+			break;
+		case ',':
+			int c = getc(stdin);
+			if (EOF != c) {
+				tape[ptr] = (uint8_t)c;
+			}
+			break;
+		case '[':
+			if (LOOP_STACK_SIZE - 1 == loop_stack_ptr) {
+				// TODO: more sophisticated handling
+				return;
+			} else if (tape[ptr] == 0) {
+				int depth = 1;
+				while (depth && ++instruction_ptr < instruction_len) {
+					if (instructions[instruction_ptr] == '[') {
+						++depth;
+					}
+					else if (instructions[instruction_ptr] == ']') {
+						--depth;
+					}
+				}
+			} else {
+				loop_stack[++loop_stack_ptr] = instruction_ptr;
+			}
+			break;
+		case ']':
+			if (-1 == loop_stack_ptr) {
+				// TODO: more sophisticated handling
+				return;
+			} else if (0 != tape[ptr]) {
+				instruction_ptr = loop_stack[loop_stack_ptr];
+			} else {
+				--loop_stack_ptr;
+			}
+			break;
+		}
+		++instruction_ptr;
+	}
 }
 
-/*** DATA ***/
+/* ================================== MAIN ================================== */
 
-unsigned char data[30000];
-int data_pointer = 0;
+int main(int argc, char **argv) {
+	FILE *fp;
+	size_t size;
+	char *buf;
 
-void incrementByte() {
-  data[data_pointer]++;
-}
+	uint8_t tape[TAPE_SIZE] = {0};
 
-void decrementByte() {
-  data[data_pointer]--;
-}
+	if (argc != 2) {
+		fprintf(stderr, "Missing file parameter\n");
+		return EXIT_FAILURE;
+	}
 
-void incrementPointer() {
-  data_pointer++;
-}
+	fp = fopen(argv[1], "r");
+	if (!fp) {
+		fprintf(stderr, "Could not open file '%s'\n", argv[1]);
+		return EXIT_FAILURE;
+	}
 
-void decrementPointer() {
-  data_pointer--;
-}
+	fseek(fp, 0, SEEK_END);
+	size = ftell(fp);
+	rewind(fp);
 
-void outputByte() {
-  putc(data[data_pointer], stdout);
-}
+	buf = malloc(size);
+	if (!buf) {
+		fprintf(stderr, "Could not allocate buffer\n");
+		return EXIT_FAILURE;
+	}
+	if (size != fread(buf, sizeof(char), size, fp)) {
+		fprintf(stderr, "Error reading the file\n");
+		return EXIT_FAILURE;
+	}
+	fclose(fp);
 
-void inputByte() {
-  data[data_pointer] = getc(stdin);
-}
+	bf_eval(tape, 0, buf, size);
 
-unsigned char peekByte() {
-  return data[data_pointer];
-}
+	free(buf);
 
-void evalInstructions(char *i, int size) {
-  int ip = 0;
-  int bc = 0;
-  
-  while (ip < size) {
-    switch (i[ip]) {
-      case '<':
-        decrementPointer();
-        break;
-      case '>':
-        incrementPointer();
-        break;
-      case '-':
-        decrementByte();
-        break;
-      case '+':
-        incrementByte();
-        break;
-      case '.':
-        outputByte();
-        break;
-      case ',':
-        inputByte();
-        break;
-      case '[':
-        bc++;
-        if (peekByte() == 0) {
-          int exitBc = bc;
-          while (1) {
-            ip++;
-            if (ip == size) die("Unmatched opening bracket!");
-            if (i[ip] == '[') {
-              bc++;
-            } else if (i[ip] == ']') {
-              if (bc == exitBc) break;
-              bc--;
-            }
-          }
-          bc--;
-        }
-        break;
-      case ']':
-        if (peekByte() != 0) {
-          int exitBc = bc;
-          while (1) {
-            ip--;
-            if (ip == 0) die("Unmatched closing bracket!");
-            if (i[ip] == ']') {
-              bc++;
-            } else if (i[ip] == '[') {
-              if (bc == exitBc) break;
-              bc--;
-            }
-          }
-        } else {
-          bc--;
-        }
-        break;
-      default:
-        die("Encountered invalid token!");
-    }
-    ip++;
-  }
-  if (bc != 0) die("Unmatched opening bracket!");
-}
-  
-int main(int argc, char *argv[]) {
-  if (argc != 2)
-    die("Invalid number of input arguments!");
-  
-  int i;
-  for (i = 0; i < DATA_SIZE; i++) 
-    data[i] = 0;
-
-  FILE *fp = fopen(argv[1], "r");
-  if (!fp) die("File could not be opened!");
-
-  char instructions[INSTRUCTIONS_SIZE + 1];
-  char c;
-  i = 0;
-  while ((c = fgetc(fp)) != EOF) {
-    if (i >= INSTRUCTIONS_SIZE) die("Too many instructions!");
-    if (c == '<' || c == '>' || c == '+' || c == '-' ||
-        c == '.' || c == ',' || c == '[' || c == ']')
-      instructions[i++] = c;
-  }
-
-  evalInstructions(instructions, i);
-  
-  return 0;
+	return 0;
 }
